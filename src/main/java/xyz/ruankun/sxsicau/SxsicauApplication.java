8ed813907ddc336139427be7e9adfc8ad5ae32fd
mayberk <qkmc@outlook.com>
2019-08-16 21:38:17 +0800

    first commit

---
+++ src/main/java/xyz/ruankun/sxsicau/SxsicauApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruankun.sxsicau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxsicauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SxsicauApplication.class, args);
+	}
+
+}
